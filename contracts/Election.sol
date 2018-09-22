2ddf45e08454b9f1f7587ceca295a8a2b9090a25
casa <diogo.alves@gmail.com>
2018-09-22 13:08:15 -0300

    feat: add voting capability

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,8 @@ contract Election {
     uint voteCount;
   }
   mapping(uint => Candidate) public candidates;
+  mapping(address => bool) public voters;
+
   uint public candidatesCount;
 
   constructor() public {
@@ -17,6 +19,13 @@ contract Election {
   function addCandidate (string _name) private {
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint _candidateId) public {
+    require(!voters[msg.sender]);
+    require(_candidateId >0 && _candidateId <= candidatesCount);
+    voters[msg.sender] = true;
+    candidates[_candidateId].voteCount ++;
   }
 
 }
